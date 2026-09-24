f4cf78e24b3a7e98900d28773e8de2b9298c3c97
expertgroup1979-glitch <expertgroup1979@gmail.com>
2026-09-25 00:26:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector(".contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    alert("Thank you! Your inquiry has been received. Please contact us on WhatsApp: 0300 4803270");
+});
